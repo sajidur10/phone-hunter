16d21348bc5527c7d881f78505e71a27a75bc16c
sajidur10 <msrrahatbd1@gmail.com>
2022-03-01 09:32:31 +0600

    ui finish

---
+++ js/api.js
@@ -0,0 +1,23 @@
+document.getElementById('error-message').style.display = 'none';
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // clear data
+    searchField.value = '';
+    document.getElementById('error-message').style.display = 'none';
+    if (searchText == '') {
+        // please write something to display
+    }
+    else {
+        // load data
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
+            .catch(error => displayError(error));
+    }
+}
+
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
+}
